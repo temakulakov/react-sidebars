fc183fed61df2586f87110f33854e17077e799d3
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2023-02-11 09:08:08 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Sidebar as Example } from "./sidebars/sidebar-6/Sidebar";
+// import { Concept as Example } from "./concepts/concept-1/Concept";
+
+function App() {
+  return <Example />;
+}
+
+export default App;
